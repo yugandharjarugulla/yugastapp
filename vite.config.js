f518a6bfd0cc41e270a8fec7305f2b49c5898d66
Yugandhar jarugulla <yugandharjarugulla2007@gmail.com>
2026-06-18 22:46:23 +0530

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Yuga_react.github.io/",
   plugins: [react()],
+  base: '/yugastapp/',
 })
